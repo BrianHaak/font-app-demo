91cc5522cc44715cb44924e341ab9dab102ee5da
Brian Haak <Haakman@users.noreply.github.com>
2019-05-20 20:45:51 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+var x = 0;
+var y = 0;
+var maxWidth = 1000;
+var maxWeight = 1000;
+
+function setStyles(property, value) {
+    return document.documentElement.querySelector(".text").style.setProperty(property, value);
+}
+
+document.addEventListener('mousemove', e => {
+
+    maxY = window.innerHeight;
+    maxX = window.innerWidth;
+    x = e.clientX;
+    y = e.clientY;
+
+    setStyles('--wdth', maxWidth * (x/maxX));
+    setStyles('--wght', maxWeight * (y/maxY));
+
+});
